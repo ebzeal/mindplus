ae005bf00a8ff293e571df89bb5a55e1e628714d
olu <ebenezerajayi@gmail.com>
2020-08-25 07:09:21 +0100

    - changes to webpack mode

---
+++ webpack.prod.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+    publicPath: '/'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: 4500,
+    historyApiFallback: {
+      disableDotRule: true
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      favicon: 'src/assets/favicon.ico'
+    })
+  ],
+  resolve: { extensions: ['.js', '.jsx'] }
+};
